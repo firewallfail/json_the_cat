f8bc2d3f483df00148f757b9195d3485ac18e012
firewallfail <firewall.fail@hotmail.com>
2022-04-14 09:57:43 -0400

    errors show before exiting, unfound search displays and exits and good search results print with a good search

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,16 @@
 const request = require('request');
+//only allow 1 search term converted to string
+const search = process.argv.slice(2, 3)[0];
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${search}`, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const parsedBody = JSON.parse(body);
+  if (!parsedBody[0]) {
+    console.error(`Breed Not Found: ${search}`);
+    return;
+  }
+  console.log(parsedBody[0].description);
+});
